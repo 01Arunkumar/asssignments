f2f7b7d92fb6a0354b46c48c2b0c7a1e0200538a
01Arunkumar <arunkumarbalure@gmail.com>
2021-03-30 23:24:24 +0530

    assignment13

---
+++ arrays1.js
@@ -0,0 +1,42 @@
+/*let car=["audi","benz","maruti","hundai","bmw","volvo","jeep"];
+console.log(car);*/
+
+/*let car=["audi","benz","maruti","hundai","bmw","volvo","jeep"];
+console.log(car[2]);*/
+
+/* let car=["audi","benz","maruti","hundai","bmw","volvo","jeep"];
+ console.log(car[car.length-1]);*/
+
+/*let car=["audi","benz","maruti","hundai","bmw","volvo","jeep"];
+for(a=0; a<=(car.length-1); a++){
+    console.log(car[a]);
+}*/
+
+//let employee=["ram","shastri","35"];
+
+/*let employee=["ram","shastri","35"];
+console.log(employee[1]);*/
+
+/*let employee=[["arun",25],["anand",34],["anil",35],["swaroopa",30]];
+console.log(employee[0]+" "+employee[1]+" "+employee[2]);*/
+
+/*let employee=[["arun",25],["anand",34],["anil",35],["swaroopa",30]];
+console.log(employee[1][1]);*/
+
+/*let employee=[["arun",25],["anand",34],["anil",35],["swaroopa",30]];
+employee.push(["sandhya",27]);
+console.log(employee);*/
+
+/*let employee=[["arun",25],["john",34],["andrew",35],["jai",30],["bachhan",27]];
+employee.splice(1,2);
+console.log(employee);*/
+
+/*let employee=[["arun",25],["john",34],["andrew",35],["jai",30],["bachhan",27]];
+employee.shift();
+console.log(employee);*/
+
+/*let employee=[["arun",25],["john",34],["andrew",35],["jai",30],["bachhan",27]];
+employee.unshift(["deo",66]);
+console.log(employee);*/
+
+
